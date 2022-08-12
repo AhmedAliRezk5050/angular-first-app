e1c9ef3b3d8c85650e7ac9046b9ef66ded5d9527
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-08-12 07:18:27 +0200

    practise - Setting the page title using resolve

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,28 @@
 import { ChildBComponent } from './pract/first-component/child-b/child-b.component';
 import { ChildAComponent } from './pract/first-component/child-a/child-a.component';
 import { NotFoundComponent } from './pract/not-found/not-found.component';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injectable, NgModule } from '@angular/core';
+import { Resolve, RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './pract/first/first.component';
 import { SecondComponent } from './pract/second/second.component';
+
+@Injectable({ providedIn: 'root' })
+export class ResolvedChildATitle implements Resolve<string> {
+  resolve() {
+    return Promise.resolve('child a');
+  }
+}
 
 const routes: Routes = [
   {
     path: 'first-component/:name',
     component: FirstComponent,
     children: [
-      { path: 'child-a', component: ChildAComponent },
+      {
+        path: 'child-a',
+        component: ChildAComponent,
+        title: ResolvedChildATitle,
+      },
       { path: 'child-b', component: ChildBComponent },
     ],
   },
