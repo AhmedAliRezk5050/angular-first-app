63e7f60743415a9ff5c2af08754b88ed2e8b665e
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-08-12 10:06:05 +0200

    Accessing query parameters and fragments

--- src/app/pract/third/third.component.ts
+++ src/app/pract/third/third.component.ts
@@ -1,15 +1,19 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-third',
   templateUrl: './third.component.html',
-  styleUrls: ['./third.component.css']
+  styleUrls: ['./third.component.css'],
 })
 export class ThirdComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe((queryParams) =>
+      console.log(queryParams['name']),
+    );
 
-  constructor() { }
-
-  ngOnInit(): void {
+    this.route.fragment.subscribe((fragment) => console.log(fragment));
   }
 
+  ngOnInit(): void {}
 }
