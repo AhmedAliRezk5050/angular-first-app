aeb4a5a4e97df881faba771dbaeb5177e9105e32
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-08-13 03:12:02 +0200

    passing static data to a route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,23 @@
-import {ChildBComponent} from './pract/first-component/child-b/child-b.component';
-import {ChildAComponent} from './pract/first-component/child-a/child-a.component';
-import {NotFoundComponent} from './pract/not-found/not-found.component';
-import {Injectable, NgModule} from '@angular/core';
+import { ChildBComponent } from "./pract/first-component/child-b/child-b.component";
+import { ChildAComponent } from "./pract/first-component/child-a/child-a.component";
+import { NotFoundComponent } from "./pract/not-found/not-found.component";
+import { Injectable, NgModule } from "@angular/core";
 import {
   Resolve,
   RouterModule,
   RouterStateSnapshot,
   Routes,
   TitleStrategy,
-} from '@angular/router';
-import {FirstComponent} from './pract/first/first.component';
-import {SecondComponent} from './pract/second/second.component';
-import {Title} from '@angular/platform-browser';
-import {ThirdComponent} from './pract/third/third.component';
-import {AuthGuard} from './pract/auth-guard.service';
-import {NotAllowedComponent} from './pract/not-allowed/not-allowed.component';
-import {CanDeactivateGuard} from "./pract/can-deactivate-guard.service";
+} from "@angular/router";
+import { FirstComponent } from "./pract/first/first.component";
+import { SecondComponent } from "./pract/second/second.component";
+import { Title } from "@angular/platform-browser";
+import { ThirdComponent } from "./pract/third/third.component";
+import { AuthGuard } from "./pract/auth-guard.service";
+import { NotAllowedComponent } from "./pract/not-allowed/not-allowed.component";
+import { CanDeactivateGuard } from "./pract/can-deactivate-guard.service";
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: "root" })
 export class TemplatePageTitleStrategy extends TitleStrategy {
   constructor(private readonly title: Title) {
     super();
@@ -31,51 +31,54 @@ export class TemplatePageTitleStrategy extends TitleStrategy {
   }
 }
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: "root" })
 export class ResolvedChildATitle implements Resolve<string> {
   resolve() {
-    return Promise.resolve('child a');
+    return Promise.resolve("child a");
   }
 }
 
 const routes: Routes = [
   {
-    path: 'first-component/:name',
+    path: "first-component/:name",
     component: FirstComponent,
     canActivateChild: [AuthGuard],
     children: [
       {
-        path: 'child-a',
+        path: "child-a",
         component: ChildAComponent,
         title: ResolvedChildATitle,
         // can be just a string
         // title: child-a,
       },
-      {path: 'child-b', component: ChildBComponent},
+      { path: "child-b", component: ChildBComponent },
     ],
   },
-  {path: 'second-component/:name', component: SecondComponent, canDeactivate: [CanDeactivateGuard]},
   {
-    path: 'third-component',
+    path: "second-component/:name",
+    component: SecondComponent,
+    canDeactivate: [CanDeactivateGuard],
+  },
+  {
+    path: "third-component",
     component: ThirdComponent,
     canActivate: [AuthGuard],
   },
   {
-    path: 'not-allowed',
+    path: "not-allowed",
     component: NotAllowedComponent,
   },
   {
-    path: '',
-    redirectTo: 'first-component/',
-    pathMatch: 'full',
+    path: "",
+    redirectTo: "first-component/",
+    pathMatch: "full",
   },
-  {path: '**', component: NotFoundComponent},
+  { path: "**", component: NotFoundComponent, data: {message: "Page is not found!"} },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: [{provide: TitleStrategy, useClass: TemplatePageTitleStrategy}],
+  providers: [{ provide: TitleStrategy, useClass: TemplatePageTitleStrategy }],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
--- src/app/pract/not-found/not-found.component.ts
+++ src/app/pract/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Data} from "@angular/router";
 
 @Component({
   selector: 'app-not-found',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./not-found.component.css']
 })
 export class NotFoundComponent implements OnInit {
+  message?: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.data.subscribe((data: Data) => {
+      this.message = data['message'];
+    })
   }
 
 }
