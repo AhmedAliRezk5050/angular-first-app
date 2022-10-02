c26185681e45dbe6771bc516743e65ff82117f43
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-10-02 14:00:45 +0200

    recipes(unsubscribe)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {Component, OnInit, Output, EventEmitter, OnDestroy} from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import {ActivatedRoute, Router} from "@angular/router";
@@ -9,7 +9,7 @@ import {Subscription} from "rxjs";
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
 
   recipesUpdatedSubscription?: Subscription
 
@@ -26,5 +26,9 @@ export class RecipeListComponent implements OnInit {
 
   createRecipe() {
     this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+  ngOnDestroy(): void {
+    this.recipesUpdatedSubscription?.unsubscribe();
   }
 }
