2585846a42095e023d762ddb121ad58efb403b12
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-10-07 18:20:49 +0200

    Dynamic Components(using ngIF)

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,50 +1,50 @@
-import {Component, OnInit} from '@angular/core';
-import {NgForm} from "@angular/forms";
-import {AuthService} from "./auth.service";
-import {HttpErrorResponse} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Router} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
   logInMode = true;
   isLoading = false;
   error: string | null = null;
 
-  constructor(private authService: AuthService, private router: Router) {
-  }
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSwitchAuthModes() {
-    this.logInMode = !this.logInMode
+    this.logInMode = !this.logInMode;
   }
 
   onSubmit(authForm: NgForm) {
-    const {email, password} = authForm.value
-    this.isLoading = true
+    const { email, password } = authForm.value;
+    this.isLoading = true;
 
-    const authObs = this.logInMode ?
-      this.authService.signIn(email, password)
-      : this.authService.signUp(email, password)
+    const authObs = this.logInMode
+      ? this.authService.signIn(email, password)
+      : this.authService.signUp(email, password);
 
     authObs.subscribe({
       next: () => {
         authForm.reset();
         this.error = null;
-        this.router.navigate(['/recipes'])
+        this.router.navigate(['/recipes']);
       },
-      error: err => {
+      error: (err) => {
         this.error = err.message;
-        this.isLoading = false
-      }
-    })
+        this.isLoading = false;
+      },
+    });
+  }
 
-
+  handleError() {
+    this.error = null;
   }
 }
