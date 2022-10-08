4c6018f294200487a398f222e4fdfad85d59e177
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-10-08 17:20:04 +0200

    Modules(Add AuthModule) => fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { AppRoutes } from './shared/AppRoutes';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
   { path: '', redirectTo: `/${AppRoutes.Recipes}`, pathMatch: 'full' },
+  { path: AppRoutes.NotFound, component: NotFoundComponent },
   { path: '**', redirectTo: AppRoutes.NotFound },
 ];
 
